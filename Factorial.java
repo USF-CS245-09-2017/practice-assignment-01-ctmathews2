0afe75d3553f2e79706604fa9119b92c78e2d6f5
Chandler Mathews <35784996+ctmathews2@users.noreply.github.com>
2019-01-25 09:55:04 -0800

    Update

--- Factorial.java
+++ Factorial.java
@@ -16,8 +16,8 @@ public class Factorial{
 	}
 
 	public int factorial_iterative(int n){
-		int result = 1, i; 
-        for (i=2; i<=n; i++) 
+		int result = 1; 
+        for (i = 2; i <= n ; i++) 
             result *= i; 
         return result; 
 	}
